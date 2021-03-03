9ec9420895f4f17579bd5eae47423abf4a2c6c34
nayanmogra <nayanmogra@gmail.com>
2021-03-03 18:32:12 +0530

    Basic Template and Templates inheritance

--- Django/django_blog/blog/views.py
+++ Django/django_blog/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author'    :   'Author1',
+        'title'     :   'My First Blog',
+        'content'   :   'First Post Content Must see',
+        'date_posted' : 'Jan 1, 2021' 
+    },
+
+        {
+        'author'    :   'Author2',
+        'title'     :   'Second Blog',
+        'content'   :   'Second Post Content Must see',
+        'date_posted' : 'Jan 10, 2021' 
+    },
+]
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html' , context = context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request , 'blog/about.html' , {'title' : "About"})
