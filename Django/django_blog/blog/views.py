41ae77f250a723da7af90986955d4023451f43a0
nayanmogra <nayanmogra@gmail.com>
2021-03-10 16:11:43 +0530

    Create Update Login Logout NewUser

--- Django/django_blog/blog/views.py
+++ Django/django_blog/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author'    :   'Author1',
-        'title'     :   'My First Blog',
-        'content'   :   'First Post Content Must see',
-        'date_posted' : 'Jan 1, 2021' 
-    },
-
-        {
-        'author'    :   'Author2',
-        'title'     :   'Second Blog',
-        'content'   :   'Second Post Content Must see',
-        'date_posted' : 'Jan 10, 2021' 
-    },
-]
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html' , context = context)
 
